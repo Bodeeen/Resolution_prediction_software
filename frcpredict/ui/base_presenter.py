04d636e71cf6b9658cc199fcd398418cf5de4751
stafak <stafak@kth.se>
2020-06-29 14:24:28 +0200

    fix: memory leak caused by presenters referencing widgets (#19)

--- frcpredict/ui/base_presenter.py
+++ frcpredict/ui/base_presenter.py
@@ -1,8 +1,8 @@
+import weakref
 from abc import abstractmethod
 from typing import TypeVar, Generic
 
 from PyQt5.QtCore import QObject
-
 
 Model = TypeVar('Model')
 
@@ -23,11 +23,13 @@ class BasePresenter(QObject, Generic[Model]):
 
     @property
     def widget(self):
-        return self._widget
+        return self._widget()
 
     # Methods
     @abstractmethod
     def __init__(self, model: Model, widget) -> None:
         super().__init__()
-        self._widget = widget
+        self._widget = weakref.ref(widget)
         self.model = model
+
+        self.setParent(widget)
