fix: wrong value when threshold line crosses two points

frcpredict/ui/output/frc/frc_results_w.py
@@ -1,17 +1,17 @@
 from functools import reduce
 from typing import Optional, List, Union
 
+import numpy as np
 import pyqtgraph as pg
 from PyQt5.QtCore import pyqtSignal, Qt, pyqtBoundSignal
 from PyQt5.QtGui import QFontMetrics
 from PyQt5.QtWidgets import QHBoxLayout, QLabel, QSlider
+from scipy.interpolate import interp1d
 
 from frcpredict.model import FrcCurve, FrcSimulationResults, FrcSimulationResultsView
 from frcpredict.ui import BaseWidget
 from frcpredict.util import get_value_from_path
 from .frc_results_p import FrcResultsPresenter
-
-from scipy.interpolate import interp1d
 
 
 class FrcResultsWidget(BaseWidget):
@@ -150,11 +150,18 @@ class FrcResultsWidget(BaseWidget):
             self._thresholdPlotItem = self.plotFrc.plot([0, 1 / 25], [threshold, threshold],
                                                         pen=pg.mkPen("r", style=Qt.DashLine))
 
-            # TODO: Fix wrong value sometimes when interpolation line crosses 2 points. Also, maybe
-            #       the interpolation logic shouldn't be here.
+            # TODO: This logic should probably not be here.
             try:
+                x = np.copy(curve.x)
+                y = np.copy(curve.y)
+                for i in range(1, len(y)):  # Prevent issues when threshold line crosses two points
+                    if y[i] > y[i - 1]:
+                        x = x[:i-1]
+                        y = y[:i-1]
+                        break
+
                 self.lblResolutionValue.setText(
-                    "%.2f nm" % (1 / interp1d(curve.y, curve.x)(threshold))
+                    "%.2f nm" % (1 / interp1d(y, x)(threshold))
                 )
             except ValueError:
                 # Probably raised because the entered threshold is outside the interpolation range