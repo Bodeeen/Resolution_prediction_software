36db59f4ee2fd9674252478f71db5dea17d19c99
stafak <stafak@kth.se>
2020-07-22 15:45:02 +0200

    improvement: colour indicators in response list (#16)

--- frcpredict/ui/input/fluorophore/response_list_item.py
+++ frcpredict/ui/input/fluorophore/response_list_item.py
@@ -1,6 +1,8 @@
+from PyQt5.QtGui import QColor, QPixmap, QIcon
 from PyQt5.QtWidgets import QListWidgetItem
 
 from frcpredict.model import IlluminationResponse
+from frcpredict.util import wavelength_to_rgb
 
 
 class ResponseListItem(QListWidgetItem):
@@ -10,13 +12,14 @@ class ResponseListItem(QListWidgetItem):
 
     # Properties
     @property
-    def wavelength(self) -> int:
+    def wavelength(self) -> float:
         return self._wavelength
 
     # Methods
     def __init__(self, response: IlluminationResponse, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
         self._wavelength = response.wavelength
+        self.setIcon(self._generateIcon(response.wavelength))
         self.setText(str(response))
 
     def __lt__(self, other: QListWidgetItem) -> bool:
@@ -24,3 +27,9 @@ class ResponseListItem(QListWidgetItem):
 
     def __gt__(self, other: QListWidgetItem) -> bool:
         return self.wavelength > other.wavelength
+
+    # Internal methods
+    def _generateIcon(self, wavelength: float) -> QIcon:
+        pixmap = QPixmap(12, 32)
+        pixmap.fill(QColor(*wavelength_to_rgb(wavelength), 255))
+        return QIcon(pixmap)
