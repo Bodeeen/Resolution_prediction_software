17b69ce593fe1754675d21ca5fcbbadb5931c773
stafak <stafak@kth.se>
2020-08-11 15:07:17 +0200

    fix: variance could be very slightly negative with fftconvolve

--- frcpredict/simulation/virtual_imaging.py
+++ frcpredict/simulation/virtual_imaging.py
@@ -7,7 +7,7 @@ def get_expected_image_from_kernels2d(kernels2d: np.ndarray,
     """ Returns the expected image based on the given sample image and simulated kernels. """
 
     exp_kernel_result = fftconvolve(sample_image_arr, kernels2d[0], mode="same")
-    var_kernel_result = fftconvolve(sample_image_arr, kernels2d[1], mode="same")
+    var_kernel_result = fftconvolve(sample_image_arr, kernels2d[1], mode="same").clip(0)
     gaussian_noise = np.random.normal(0, np.sqrt(var_kernel_result))
 
     expected_image = exp_kernel_result + gaussian_noise
